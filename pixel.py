6a0b1bf120a2cbf92a6d4e1568ce4244ee172a09
Evan <evan@evanw.org>
2018-10-24 02:46:27 +0000

    correct resolution, add xinitrc

--- pixel.py
+++ pixel.py
@@ -9,12 +9,8 @@ import threading
 from tkinter import *
 master = Tk()
 
-# w = Canvas(master, width=1600, height=900)
-# w.create_rectangle(0, 0, 100, 100, fill="blue", outline = 'blue')
-# w.create_rectangle(50, 50, 100, 100, fill="red", outline = 'blue')
-# w.pack()
-# master.mainloop()
-
+screen_width = 1280
+screen_height = 1024
 
 class TkApp(threading.Thread):
 
@@ -28,7 +24,7 @@ class TkApp(threading.Thread):
     def run(self):
         self.root = tk.Tk()
         self.root.protocol("WM_DELETE_WINDOW", self.callback)
-        self.w = tk.Canvas(self.root, width=1600, height=900)
+        self.w = tk.Canvas(self.root, width=screen_width, height=screen_height)
         # self.w.create_rectangle(0, 0, 100, 100, fill="blue", outline = 'blue')
         # self.w.create_rectangle(50, 50, 100, 100, fill="red", outline = 'blue')
         self.w.pack()
@@ -45,11 +41,9 @@ logging.basicConfig(level=logging.INFO, format='%(message)s')
 app = Flask(__name__)
 limiter = Limiter(app, key_func=get_remote_address)
 
-screen_width = 1600
-screen_height = 900
 
-display_width = 16
-display_height = 16
+display_width = 128
+display_height = 128
 
 pixel_width = screen_width / display_width
 pixel_height = screen_height / display_height
