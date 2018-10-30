094fd94bce87a524bc032ba8096a88552b636fa7
evan <evan@evanw.org>
2018-10-29 20:43:31 -0500

    add twisted version

---
+++ pixel_twisted.py
@@ -0,0 +1,72 @@
+from twisted.web.server import Site
+from twisted.web.resource import Resource
+from twisted.internet import reactor
+import logging
+import tkinter as tk
+import threading
+from tkinter import *
+master = Tk()
+
+screen_width = 1280
+screen_height = 1024
+
+display_width = 128
+display_height = 128
+
+pixel_width = screen_width / display_width
+pixel_height = screen_height / display_height
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+class TkApp(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.start()
+
+    def callback(self):
+        self.root.quit()
+
+    def run(self):
+        self.root = tk.Tk()
+        self.root.protocol("WM_DELETE_WINDOW", self.callback)
+        self.w = tk.Canvas(self.root, width=screen_width, height=screen_height)
+        self.w.pack()
+
+        label = tk.Label(self.root, text="Hello World")
+        label.pack()
+
+        self.root.mainloop()
+
+class Page(Resource):
+    isLeaf = True
+    def render_POST(self, request):
+        try:
+            x = int(request.getHeader('x'))
+            y = int(request.getHeader('y'))
+            color = request.getHeader('color')
+
+            print(x, y, color)
+
+            tkapp.w.create_rectangle(
+                x * pixel_width,
+                y * pixel_height,
+                x * pixel_width + pixel_width,
+                y * pixel_height + pixel_height,
+                fill=color,
+                width=0
+            )
+        except Exception:
+            return b"you suck\n"
+        finally:
+            return b"success\n"
+    def render_GET(self, request):
+        print(request.getHeader('x'))
+        return b'success\n'
+
+
+tkapp = TkApp()
+
+site = Site(Page())
+reactor.listenTCP(80, site)
+reactor.run()
